86bc8ade1b6199ca3ae7de53e12d24c6dcc0b415
TheLoneK <99369545+TheLoneK@users.noreply.github.com>
2026-09-09 14:38:41 -0600

    updated copyright

--- Source/GenericPingSystem/GenericPingSystem.Build.cs
+++ Source/GenericPingSystem/GenericPingSystem.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+
 using UnrealBuildTool;
 
 public class GenericPingSystem : ModuleRules
--- Source/GenericPingSystem/Private/GPS_PingComponent.cpp
+++ Source/GenericPingSystem/Private/GPS_PingComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//Copyright Levon 2026. All Rights Reserved
 
 
 #include "GPS_PingComponent.h"
